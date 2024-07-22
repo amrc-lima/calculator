74c055454cf031c20114202ee9d6e45e5c62dcae
Eduardo Lima <eduardoamerico2009@gmail.com>
2024-07-22 19:40:22 -0300

    added every remaining button, put all of them in order

--- main.py
+++ main.py
@@ -1,44 +1,210 @@
 from tkinter import Tk, Entry, Button, Frame
 
-vscode_gray = "#1f1f1f"
-darker_gray = '#101010'
+button_gray = '#373737'
+background_gray = '#2c2c2c'
+lighter_gray = '#4f4f4f'
+highlight_gray = '#181818'
 darker_white = '#cecece'
 
 def main():
     window = Tk()
     window.title("Calculator")
-    window.minsize(362,463)
-    window.configure(background = vscode_gray)
+    window.minsize(288, 275)
+    window.configure(background = background_gray)
+    window.grid_columnconfigure(0, weight = 1)
+    window.grid_rowconfigure(1, weight = 1)
     
     frame1 = Frame(window)
-    frame1.configure(bg = vscode_gray)
-    frame1.grid(column = 0, row = 0)
+    frame1.configure(bg = background_gray)
+    frame1.grid(sticky = 'nsew')
+    frame1.grid_columnconfigure(0, weight = 1)
 
     frame2 = Frame(window)
-    frame2.configure(bg = vscode_gray)
-    frame2.grid(column = 0, row = 1)
+    frame2.configure(bg = background_gray)
+    frame2.grid(sticky = 'nsew')
 
-    operation = Entry(
+    for n in range(4):
+        frame2.grid_columnconfigure(n, weight = 1)
+
+    for n in range(6):
+        frame2.grid_rowconfigure(n, weight = 1)
+
+    padding = 1
+    thickness = 1
+
+    Entry(
         frame1,
-        bg = vscode_gray, 
+        bg = '#272727', 
         fg = 'white', 
         font = ('Trebuchet', 16), 
-        highlightbackground = darker_white, 
-        highlightthickness = 2,
-        )
-    operation.grid(sticky = 'ew')
+        highlightthickness = thickness, 
+        highlightbackground = highlight_gray,
+        relief = 'flat', 
+        ).grid(sticky = 'nsew', pady = padding)
 
-    for n in range(10): 
-        new_button = Button(
+
+    Button(
+        frame2, 
+        bg = '#991121', 
+        activebackground = '#b93638', 
+        fg = 'white', 
+        activeforeground = darker_white, 
+        highlightbackground = highlight_gray,
+        highlightthickness = thickness,
+        relief = 'flat',
+        text = '⌫', 
+        font = ('Trebuchet', 14), 
+        width = 3,
+        ).grid(row = 0, column = 0, padx = padding, pady = padding, sticky = 'nsew')
+
+    Button(
+        frame2, 
+        bg = button_gray, 
+        activebackground = lighter_gray, 
+        fg = 'white', 
+        activeforeground = darker_white, 
+        highlightbackground = highlight_gray,
+        highlightthickness = thickness,
+        relief = 'flat',
+        text = 'C', 
+        font = ('Trebuchet', 14, 'bold'), 
+        width = 3,
+        ).grid(row = 0, column = 1, padx = padding, pady = padding, sticky = 'nsew')
+    
+
+    pos_column = 2
+    for n in ['(',')']:
+        Button(
             frame2, 
-            bg = vscode_gray, 
-            activebackground = darker_gray, 
+            bg = button_gray, 
+            activebackground = lighter_gray, 
             fg = 'white', 
             activeforeground = darker_white, 
+            highlightbackground = highlight_gray,
+            highlightthickness = thickness,
+            relief = 'flat',
             text = n, 
-            width = 5,
-            )
-        new_button.grid(row = (n//3), column = (n % 3), sticky = 'nsew')
+            font = ('Trebuchet', 14), 
+            width = 3,
+            ).grid(row = 0, column = pos_column, padx = padding, pady = padding, sticky = 'nsew')
+        pos_column += 1
+        
+    pos_row = 1
+    for n in ['+','-','/','*']:
+        Button(
+            frame2, 
+            bg = '#b67000', 
+            activebackground = '#ed9e23', 
+            fg = 'white', 
+            activeforeground = 'black', 
+            highlightbackground = highlight_gray,
+            highlightthickness = thickness,
+            relief = 'flat',
+            text = n, 
+            font = ('Trebuchet', 14), 
+            width = 3,
+            ).grid(row = pos_row, column = 3, padx = padding, pady = padding, sticky = 'nsew')
+        pos_row += 1
+
+    Button(
+        frame2, 
+        bg = button_gray, 
+        activebackground = lighter_gray, 
+        fg = 'white', 
+        activeforeground = darker_white, 
+        highlightbackground = highlight_gray, 
+        highlightthickness = thickness, 
+        relief = 'flat', 
+        text = ',', 
+        font = ('Trebuchet', 14), 
+        width = 3, 
+        ).grid(row = 4, column = 2, padx = padding, pady = padding, sticky = 'nsew')
+
+    Button(
+        frame2, 
+        bg = button_gray, 
+        activebackground = lighter_gray, 
+        fg = 'white', 
+        activeforeground = darker_white, 
+        highlightbackground = highlight_gray, 
+        highlightthickness = thickness, 
+        relief = 'flat', 
+        text = '%', 
+        font = ('Trebuchet', 14), 
+        width = 3, 
+        ).grid(row = 4, column = 1, padx = padding, pady = padding, sticky = 'nsew')
+
+    Button(
+        frame2, 
+        bg = button_gray, 
+        activebackground = lighter_gray, 
+        fg = 'white', 
+        activeforeground = darker_white, 
+        highlightbackground = highlight_gray, 
+        highlightthickness = thickness, 
+        relief = 'flat', 
+        text = '√', 
+        font = ('Trebuchet', 14), 
+        width = 3, 
+        ).grid(row = 5, column = 0, padx = padding, pady = padding, sticky = 'nsew')
+
+    Button(
+        frame2, 
+        bg = button_gray, 
+        activebackground = lighter_gray, 
+        fg = 'white', 
+        activeforeground = darker_white, 
+        highlightbackground = highlight_gray, 
+        highlightthickness = thickness, 
+        relief = 'flat', 
+        text = '^', 
+        font = ('Trebuchet', 14), 
+        width = 3, 
+        ).grid(row = 5, column = 1, padx = padding, pady = padding, sticky = 'nsew')
+
+    Button(
+        frame2, 
+        bg = '#0d761d', 
+        activebackground = '#3d993e', 
+        fg = 'white', 
+        activeforeground = darker_white, 
+        highlightbackground = highlight_gray, 
+        highlightthickness = thickness, 
+        relief = 'flat', 
+        text = '=', 
+        font = ('Trebuchet', 14),
+        ).grid(row = 5, column = 2, padx = padding, pady = padding, sticky = 'nsew', columnspan=2)
+
+
+
+        
+    row_config = [(7,8,9), (4,5,6), (1,2,3), (0,)]
+    # These tuples will provide a look more similar to a standard calculator and save some lines of code
+
+    for x in row_config:
+        for n in x:
+            pos_column = n - x[0]
+            pos_row  = row_config.index(x)
+            Button(
+                frame2, 
+                bg = button_gray, 
+                activebackground = lighter_gray, 
+                fg = 'white', 
+                activeforeground = darker_white, 
+                highlightbackground = highlight_gray,
+                highlightthickness = thickness,
+                relief = 'flat',
+                text = n, 
+                font = ('Trebuchet', 14), 
+                width = 3,
+                ).grid(
+                    row = pos_row + 1, 
+                    column = pos_column, 
+                    sticky = 'nsew', 
+                    padx = padding, 
+                    pady = padding, 
+                    )
+
 
 
 
